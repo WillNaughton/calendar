f6cf498172c9b08eaac5c4aa60e75275e579fccc
willnaughton <banjo.ninja123@gmail.com>
2019-04-22 22:00:09 -0700

    updated calendar solution

--- js/calender.js
+++ js/calender.js
@@ -1,85 +1,125 @@
-var daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+$(document).ready(function() {
 
-var monthsOfTheYear = ['January', 'February', 'March', 'April',
-    'May', 'June', 'July', 'August', 'September',
-    'October', 'November', 'December'];
-
-var daysOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var currentDate = new Date();
-var currentMonth = currentDate.getMonth();
-var monthName;
-var month;
-var year;
-console.log(currentDate);
-console.log(currentMonth);
-var modDate = new Date();
-
-//calendar constructor
-function Calendar(month, year) {
-    this.month = (isNaN(month) || month == null) ? currentDate.getMonth() : month;
-    this.year = (isNaN(year) || year == null) ? currentDate.getFullYear() : year;
-    this.html = ' ';
-}
-
-Calendar.prototype.generateHTML = function () {
-    var firstDay = new Date(this.year, this.month, 1);
-    var startingDay = firstDay.getDay();
-    var monthLength = daysOfTheMonth[this.month];
-
-    // this is to calculate leap year
-    if (this.month == 1) {
-        if ((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0) {
-            monthLength = 29;
+    $(".center-cal .cal").each(function(e) {
+        if (e != 3) {
+            $(this).hide();
         }
-    }
+    });
 
-    
 
-    monthName = monthsOfTheYear[this.month];
-    var html = '<table class="calendar table">';
-    html += '<tr><th colspan="7">';
-    html += monthName + "&nbsp" + this.year;
-    html += '</th></tr>';
-    html += '<tr class="header">';
-
-    for (var i = 0; i <= 6; i++) {
-        html += '<td class="header-day">';
-        html += daysOfTheWeek[i];
-        html += '</td>';
-    }
-    html += '</tr><tr>';
-
-    var day = 1;
-    // this is for weeks
-    for (var i = 0; i < 9; i++) {
-        // this is for weekdays
-        for (var j = 0; j <= 6; j++) {
-            html += '<td class="calendar-day">';
-            if (day <= monthLength && (i > 0 || j >= startingDay)) {
-                html += day;
-                day++;
-            }
-            html += '</td>';
-        }
-        // stop making rows if we've run out of days
-        if (day > monthLength) {
-            break;
+    $(".next").click(function() {
+        if ($(".center-cal .cal:visible").next().length != 0) {
+            $('.center-cal .cal:visible').next().show().prev().hide();
         } else {
-            html += '</tr><tr>';
+            $(".center-cal .cal:visible").hide();
+            $(".center-cal .cal:first").show();
         }
-    }
+        return false;
+    });
 
-    html += '</tr></table>';
+    $(".prev").click(function() {
+        if ($(".center-cal .cal:visible").prev().length != 0) {
+            $('.center-cal .cal:visible').prev().show().next().hide();
+        } else {
+            $(".center-cal .cal:visible").hide();
+            $(".center-cal .cal:last").show();
+        }
+        return false;
+    });
 
-    this.html = html;
-}
 
-Calendar.prototype.getHTML = function() {
-    return this.html;
-}
-var cal = new Calendar();
 
-//call the calendar function
-cal.generateHTML(modDate);
-document.getElementById('cal').innerHTML = cal.getHTML();
+})
 
+
+
+
+// var daysOfTheWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+// var monthsOfTheYear = ['January', 'February', 'March', 'April',
+//     'May', 'June', 'July', 'August', 'September',
+//     'October', 'November', 'December'];
+
+// var daysOfTheMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+// var currentDate = new Date();
+// var currentMonth = currentDate.getMonth();
+// var monthName;
+// var month;
+// var year;
+// console.log(currentDate);
+// console.log(currentMonth);
+// var modDate = new Date();
+// var i;
+
+//   function Calendar(month, year) {
+//         this.month = (isNaN(month) || month == null) ? currentDate.getMonth() : month;
+//         this.year = (isNaN(year) || year == null) ? currentDate.getFullYear() : year;
+//         this.html = ' ';
+//     }
+
+//     for (month = 0; month < 12; month++) {
+//     //calendar constructor
+  
+//     Calendar.prototype.generateHTML = function () {
+//         var firstDay = new Date(this.year, this.month, 1);
+//         var startingDay = firstDay.getDay();
+//         var monthLength = daysOfTheMonth[this.month];
+
+//         // this is to calculate leap year
+//         if (this.month == 1) {
+//             if ((this.year % 4 == 0 && this.year % 100 != 0) || this.year % 400 == 0) {
+//                 monthLength = 29;
+//             }
+//         }
+
+
+
+//         monthName = monthsOfTheYear[this.month];
+//         var html = '<table class="calendar table">';
+//         html += '<tr><th colspan="7">';
+//         html += monthName + "&nbsp" + this.year;
+//         html += '</th></tr>';
+//         html += '<tr class="header">';
+
+//         for (var i = 0; i <= 6; i++) {
+//             html += '<td class="header-day">';
+//             html += daysOfTheWeek[i];
+//             html += '</td>';
+//         }
+//         html += '</tr><tr>';
+
+//         var day = 1;
+//         // this is for weeks
+//         for (var i = 0; i < 9; i++) {
+//             // this is for weekdays
+//             for (var j = 0; j <= 6; j++) {
+//                 html += '<td class="calendar-day">';
+//                 if (day <= monthLength && (i > 0 || j >= startingDay)) {
+//                     html += day;
+//                     day++;
+//                 }
+//                 html += '</td>';
+//             }
+//             // stop making rows if we've run out of days
+//             if (day > monthLength) {
+//                 break;
+//             } else {
+//                 html += '</tr><tr>';
+//             }
+//         }
+
+//         html += '</tr></table>';
+
+//         this.html = html;
+//     }
+
+//     Calendar.prototype.getHTML = function () {
+//         return this.html;
+//     }
+//     var cal = new Calendar();
+
+//     //call the calendar function
+//     cal.generateHTML(modDate);
+//     document.getElementById('cal').innerHTML = cal.getHTML();
+
+// }
